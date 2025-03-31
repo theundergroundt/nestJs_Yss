1c7d8af14435b6683b05c16332f5958e7c57c771
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-03-31 22:22:25 +0900

    은닉과 캡슐(추가)

--- nestjs_practice_proj/src/app.controller.ts
+++ nestjs_practice_proj/src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Req, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
 
   @Get()
   getHello(): string {
-    console.log('hello world');
-    return 'Hello world';
+    return this.catsService.hiCatserviceProduct();
   }
 }
--- nestjs_practice_proj/src/app.module.ts
+++ nestjs_practice_proj/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [CatsModule], // 모듈 여러개 추가 가능
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], // 프로바이더에 있는것을 위 컨트롤러가 사용 가능
 })
 export class AppModule {}
